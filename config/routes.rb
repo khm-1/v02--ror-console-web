0412fe7b75380bfc8b7e860ecaa6e71ee5272935
Your Name <you@example.com>
2025-05-26 02:49:22 +0000

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,13 @@ Rails.application.routes.draw do
 
   root "page#home"
 
+  get "page/about", as: :about
+  # resources :posts, only: [:home, :about], controller: "page"
+  resources :posts
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  get "page/home", as: :home
-  get "page/about", as: :about
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
